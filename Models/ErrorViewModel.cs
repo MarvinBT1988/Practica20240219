f88140c0a493b01432a94323023da075212d3690
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2024-02-19 08:18:22 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practica20240219.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
